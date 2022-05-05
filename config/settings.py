bb901167e4faf2465f27b47f6abdc170217c044f
Rui <66931280+zackeryzhou@users.noreply.github.com>
2022-05-05 20:59:18 +0800

    changed url from Core to core

--- config/settings.py
+++ config/settings.py
@@ -51,7 +51,7 @@ INSTALLED_APPS = [
     'drf_yasg',
     'users',
     'Core',
-    'Trace',
+    
 ]
 
 MIDDLEWARE = [
--- config/urls.py
+++ config/urls.py
@@ -35,8 +35,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('Core/',include('Core.urls')),
-    path('Trace/', include('Trace.urls')),
-    path('Core/APIDoc', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('core/',include('Core.urls')),
+    path('core/APIDoc', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
