3e8816ce10e0725d49f8493ce3aadf9d883ccb75
Rui <66931280+zackeryzhou@users.noreply.github.com>
2022-05-05 21:05:47 +0800

    changed url from Core to core

--- Core/urls.py
+++ Core/urls.py
@@ -8,8 +8,8 @@ router.register(r'ExitEntry', ExitEntryViewSet)
 router.register(r'members', hkuMembersViewSet)
 router.register(r'venues', VenuesViewset)
 
-#e.g. http://127.0.0.1:8000/Core/members/close-contacts/3030012345/20220411/
-#e.g. http://127.0.0.1:8000/Core/venues/infectious-venues/3030012345/20220411/
+#e.g. http://127.0.0.1:8000/core/members/close-contacts/3030012345/20220411/
+#e.g. http://127.0.0.1:8000/core/venues/infectious-venues/3030012345/20220411/
 
 urlpatterns = [
     path('venues/infectious-venues/<str:hkuID>/<str:date>/', ContactVenue),
