f2a22b9fb9e641673876670f916f7597c39107cb
Rui <66931280+zackeryzhou@users.noreply.github.com>
2022-05-05 22:27:30 +0800

    set DEBUG=True for testing

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env.str('QDD_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['group-k-studysafe.herokuapp.com', 'localhost', '127.0.0.1']
 
